65f388d99b7faea858bfb588dc0edb085a46e364
partha-sharma <parthas.ratul@gmail.com>
2025-09-07 04:26:01 +0600

    feat(frontend): Create UI and connect Login page to API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+// Import the component you just created
+import Register from './components/Register';
 
 function App() {
   return (
-    <div className="App">
-      <h1>AUST Blood Donor Platform</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <Routes>
+            {/* This line tells the app: when the URL is "/register", show the Register component */}
+            <Route path="/register" element={<Register />} />
+            
+            {/* You can add a temporary Home route as well */}
+            <Route path="/" element={<h1>Welcome to the AUST Blood Donor Platform</h1>} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
