fefb50bf2752c4edacb43ae30e0989867028cbf6
mahdiazfar27 <mahdi.azfar.talukder@gmail.com>
2025-09-19 20:18:36 +0600

    (backend): implemented API endpoint to fetch all blood requests

--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -1,11 +1,14 @@
 // backend/routes/requestRoutes.js
 const express = require('express');
 const router = express.Router();
-const { createRequest } = require('../controllers/requestController');
+
+const { createRequest, getRequests } = require('../controllers/requestController');
 const { protect } = require('../middleware/auth');
 
-// @route   POST /api/requests
-// This route is protected, so only logged-in users can access it.
+//route for getting all requests
+router.get('/', protect, getRequests);
+
+// route for creating a request
 router.post('/', protect, createRequest);
 
 module.exports = router;
